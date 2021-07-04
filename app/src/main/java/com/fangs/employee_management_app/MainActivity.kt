69f6f6d5dcb41267f551e412fd5a890f2b5af109
Fangszx <jygrzn@gmail.com>
2021-07-04 19:47:14 +0800

    Initial commit

---
+++ app/src/main/java/com/fangs/employee_management_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fangs.employee_management_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
