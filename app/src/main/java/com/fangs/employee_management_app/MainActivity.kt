442b4041b81178a5a54d5117e711e46c006f24be
Fangszx <jygrzn@gmail.com>
2021-07-04 20:00:20 +0800

    test button

--- app/src/main/java/com/fangs/employee_management_app/MainActivity.kt
+++ app/src/main/java/com/fangs/employee_management_app/MainActivity.kt
@@ -3,29 +3,26 @@ package com.fangs.employee_management_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    //bind all view
-    private val etName = findViewById<EditText>(R.id.et_main_name)
-    private val etEmail = findViewById<EditText>(R.id.et_main_email)
-    //button
-    private val btnAddToRecord = findViewById<Button>(R.id.btn_add_to_record)
+    private lateinit var btnAddToRecord : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAddToRecord = findViewById(R.id.btn_add_to_record)
 
         btnAddToRecord.setOnClickListener {
 
             Toast.makeText(applicationContext, "btn clicked", Toast.LENGTH_SHORT).show()
 
         }
-
-
     }
+
+
 
 
 
