c9b057293cfdb432472d471f8a6fd463138c2e81
Fangszx <jygrzn@gmail.com>
2021-07-05 12:20:39 +0800

    add item layout

--- app/src/main/java/com/fangs/employee_management_app/EmployeeAdapter.kt
+++ app/src/main/java/com/fangs/employee_management_app/EmployeeAdapter.kt
@@ -1,4 +1,27 @@
 package com.fangs.employee_management_app
 
-class EmployeeAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class EmployeeAdapter(private val context : Context, private val employeeList : ArrayList<EmployeeModel>) : RecyclerView.Adapter<EmployeeAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_view,parent,null)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int = employeeList.size
+
 }
